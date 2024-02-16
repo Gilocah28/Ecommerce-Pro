b497afdf7fa3e3e1488632593bd2506f74afdaa3
Gibermar Regalado <gberxx@gmail.com>
2024-02-16 14:41:00 +0800

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/navbar/Navbar'
+import MainContainer from './components/main/MainContainer'
+
+const App = () => {
+  return (
+    <div  className='App'>
+      <Navbar />
+      <MainContainer/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/main/MainContainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ImageGrid from './imageSection/ImageGrid'
+import Title from './textTitle/Title'
+
+const MainContainer = () => {
+  return (
+    <div>
+      <ImageGrid/>
+      <Title/>
+    </div>
+  )
+}
+
+export default MainContainer
